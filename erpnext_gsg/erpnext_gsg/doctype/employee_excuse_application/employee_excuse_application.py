33924108f3cfa034e227cdc13601c4984765a5e1
zaqoutabed <abdullahzaqout20@gmail.com>
2023-03-09 14:16:01 +0200

    16: check total excuse hours allowed in current month

--- erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.py
+++ erpnext_gsg/erpnext_gsg/doctype/employee_excuse_application/employee_excuse_application.py
@@ -3,28 +3,53 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import time_diff_in_hours
+from frappe.utils import time_diff_in_hours, get_first_day, get_last_day
 
 
 class EmployeeExcuseApplication(Document):
     def validate(self):
-        # Check if the employee has already excused the application
-        if (
-            len(
-                frappe.get_list(
-                    "Employee Excuse Application",
-                    filters={
-                        "employee": self.employee,
-                        "name": ["!=", self.name],
-                        "excuse_date": self.excuse_date,
-                        "docstatus": 1,
-                    },
-                )
-            )
-            > 0
-        ):
-            frappe.throw("This employee has already excuse application")
-
         if time_diff_in_hours(self.from_time, self.to_time) > 0:
             frappe.throw("From time should before To time")
         self.hours = time_diff_in_hours(self.to_time, self.from_time)
+        self.validate_allowed_hours()
+
+    def on_submit(self):
+        self.validate_allowed_hours()
+
+    def validate_allowed_hours(self):
+        current_employee_department = frappe.db.get_value(
+            "Employee", self.employee, "department"
+        )
+        if not current_employee_department:
+            return
+        department_hours = (
+            frappe.db.get_value(
+                "Department", current_employee_department, "excuse_hours_allowed"
+            )
+            or 0
+        )
+
+        if department_hours == 0:
+            return
+
+        month_start = get_first_day(self.excuse_date)
+        month_end = get_last_day(self.excuse_date)
+
+        total_hours_in_month = frappe.db.sql(
+            f"""
+            SELECT IFNULL(sum(hours), 0) as hours FROM `tabEmployee Excuse Application`
+            WHERE employee='{self.employee}'
+            AND department='{current_employee_department}'
+            AND excuse_date BETWEEN '{month_start}' AND '{month_end}'
+            AND docstatus = 1
+        """,
+            as_dict=True,
+        )
+        total_hours_in_month = (
+            total_hours_in_month[0].hours if len(total_hours_in_month) > 0 else 0
+        )
+
+        if total_hours_in_month > department_hours:
+            frappe.throw(
+                f"<b>{total_hours_in_month}</b> hours for employee Employee <b>{self.employee}</b> exceeded allowed hours in department <b>{current_employee_department} {department_hours}</b>"
+            )
